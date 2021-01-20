890ea53cc2ebb72d7b0a9a30ac1cec06ecace8c5
Natalia Braz <naabraz@gmail.com>
2021-01-20 10:41:00 -0300

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Natalia Braz on 20/01/21.
+//  Copyright © 2021 Natalia Braz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
