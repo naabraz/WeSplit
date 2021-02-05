008f1f426def03949769ebc5e1a712a49d643144
Natalia Braz <naabraz@gmail.com>
2021-02-05 20:35:07 -0300

    challenge 1

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -1,65 +1,64 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Natalia Braz on 20/01/21.
+//  Copyright © 2021 Natalia Braz. All rights reserved.
+//
+
 import SwiftUI
 
 struct ContentView: View {
-    @State private var checkAmount = ""
-    @State private var numberOfPeople = ""
-    @State private var tipPercentage = 2
+  @State private var checkAmount = ""
+  @State private var numberOfPeople = 2
+  @State private var tipPercentage = 2
+  
+  let tipPercentages = [10, 15, 20, 25, 0]
+  
+  var totalPerPerson: Double {
+    let peopleCount = Double(numberOfPeople + 2)
+    let tipSelection = Double(tipPercentages[tipPercentage])
+    let orderAmount = Double(checkAmount) ?? 0
     
-    let tipPercentages = [10, 15, 20, 25, 0]
-        
-    var totalPerPerson: Double {
-        let numberOfPeopleDouble = Double(numberOfPeople) ?? 0
-        let peopleCount = Double(numberOfPeopleDouble)
-        let tipSelection = Double(tipPercentages[tipPercentage])
-        let orderAmount = Double(checkAmount) ?? 0
-
-        let tipValue = orderAmount / 100 * tipSelection
-        let grandTotal = orderAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
-        
-        return amountPerPerson
-    }
+    let tipValue = orderAmount / 100 * tipSelection
+    let grandTotal = orderAmount + tipValue
+    let amountPerPerson = grandTotal / peopleCount
     
-    var totalAmountForCheck: Double {
-        let tipSelectionX = Double(tipPercentages[tipPercentage])
-        let orderAmountX = Double(checkAmount) ?? 0
-        let totalAmountForCheckX = orderAmountX / 100 * tipSelectionX
-        
-        return totalAmountForCheckX
-    }
-    
-    var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    TextField("Amount", text: $checkAmount)
-                        .keyboardType(.decimalPad)
-                    
-                    TextField("Number of people", text: $numberOfPeople)
-                        .keyboardType(.numberPad)
-                }
-                Section(header: Text("How much tip do you want to leave?")) {
-                    Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(0 ..< tipPercentages.count) {
-                            Text("\(self.tipPercentages[$0])%")
-                        }
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                }
-                Section(header: Text("Amount per person")) {
-                    Text("$\(totalPerPerson, specifier: "%.2f")")
-                }
-                Section(header: Text("Total amount for the check")) {
-                    Text("$\(totalAmountForCheck, specifier: "%.2f")")
-                }
+    return amountPerPerson
+  }
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section {
+          TextField("Amount", text: $checkAmount)
+            .keyboardType(.decimalPad)
+          
+          Picker("Number of people", selection: $numberOfPeople) {
+            ForEach(2 ..< 100) {
+              Text("\($0) people")
             }
-            .navigationBarTitle("WeSplit")
+          }
         }
+        Section(header: Text("How much tip do you want to leave?")) {
+          Picker("Tip percentage", selection: $tipPercentage) {
+            ForEach(0 ..< tipPercentages.count) {
+              Text("\(self.tipPercentages[$0])%")
+            }
+          }
+          .pickerStyle(SegmentedPickerStyle())
+        }
+        Section(header: Text("Amount per person")) {
+          Text("$\(totalPerPerson, specifier: "%.2f")")
+        }
+      }
+      .navigationBarTitle("We Split")
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
