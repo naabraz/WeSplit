2d346c2aff463a2b73f8bf82beb904e6a7222c63
Natalia Braz <naabraz@gmail.com>
2021-02-05 21:10:36 -0300

    challenge 3

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -2,20 +2,20 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var checkAmount = ""
-  @State private var numberOfPeople = 2
+  @State private var numberOfPeople = ""
   @State private var tipPercentage = 2
   
   let tipPercentages = [10, 15, 20, 25, 0]
   
   var totalPerPerson: Double {
-    let peopleCount = Double(numberOfPeople + 2)
+    let peopleCount = Double(numberOfPeople) ?? 2
     let tipSelection = Double(tipPercentages[tipPercentage])
     let orderAmount = Double(checkAmount) ?? 0
     
     let tipValue = orderAmount / 100 * tipSelection
     let grandTotal = orderAmount + tipValue
     let amountPerPerson = grandTotal / peopleCount
-    
+
     return amountPerPerson
   }
   
@@ -35,12 +35,8 @@ struct ContentView: View {
         Section {
           TextField("Amount", text: $checkAmount)
             .keyboardType(.decimalPad)
-          
-          Picker("Number of people", selection: $numberOfPeople) {
-            ForEach(2 ..< 100) {
-              Text("\($0) people")
-            }
-          }
+          TextField("Number of people", text: $numberOfPeople)
+            .keyboardType(.decimalPad)
         }
         Section(header: Text("How much tip do you want to leave?")) {
           Picker("Tip percentage", selection: $tipPercentage) {
